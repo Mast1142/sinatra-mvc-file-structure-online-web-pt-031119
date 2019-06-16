04547d96dcac74a86b849f0be930b0920c72d962
Samuel T <ssamt1142@gmail.com>
2019-06-16 16:52:55 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,15 +1,15 @@
 require 'pry'
 class Dog
   attr_accessor :name, :breed, :age
-  @all = []
+  @@all = []
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @all << self
+    @@all << self
   end
 
   def all
-    @all
+    @@all
   end
 end
